11dda67a5e73e7868146900827836959bc759a8f
Enzo <115178427+Sinoqa@users.noreply.github.com>
2023-02-12 17:56:18 -0800

    Update users.py

--- app1/flask_app/controllers/users.py
+++ app1/flask_app/controllers/users.py
@@ -8,7 +8,26 @@ def index():
     return render_template('index.html', users = all_users)
 
 
+@app.route('/users/update/<id>', methods = ['POST', 'GET'])
+def update_user(id):
+    if request.method == 'POST':
+        user.User.update_user(request.form)
+        return redirect('/')
+    if request.method == 'GET':
+        this_user = user.User.get_user_id(id)
+        return render_template ('edit_user.html', user  = this_user)
+
+
+@app.route('/users/create', methods=['POST', 'GET'])
+def create_user():
+    if request.method == 'POST':
+        user.User.create_user(request.form)
+        return redirect('/')
+    if request.method =='GET':
+        return render_template('create_user.html')
+
 @app.route('/users/delete/<id>')
 def delete_user(id):
     user.User.delete_user(id)
     return redirect ('/')
+
