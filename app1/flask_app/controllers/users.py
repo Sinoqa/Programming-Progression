45323b28e2eaf32b469e6a9329d802263e5e1477
Enzo <115178427+Sinoqa@users.noreply.github.com>
2023-02-13 21:27:38 -0800

    Update users.py

--- app1/flask_app/controllers/users.py
+++ app1/flask_app/controllers/users.py
@@ -1,11 +1,17 @@
 from flask_app import app
 from flask import render_template, redirect, request, session, flash
-from flask_app.models import user
+from flask_app.models import user, book
 
 @app.route('/')
 def index():
     all_users = user.User.get_all_users()
     return render_template('index.html', users = all_users)
+
+
+@app.route('/users/profile/<id>')
+def show_user_profile(id):
+    this_user = book.Book.get_all_books_by_user(id)   
+    return render_template('profile.html', user = this_user)
 
 
 @app.route('/users/update/<id>', methods = ['POST', 'GET'])
