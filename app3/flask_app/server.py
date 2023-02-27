fa0e4aecae4c3452ca9953355e98a9366db8136c
Enzo <115178427+Sinoqa@users.noreply.github.com>
2023-02-26 22:06:51 -0800

    Update server.py

--- app3/flask_app/server.py
+++ app3/flask_app/server.py
@@ -1,5 +1,11 @@
-from flask_app import app
-from flask_app.controllers import user
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template ('home.html')
+
 
 
 if __name__ == "__main__":
